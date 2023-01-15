a8551db16fff7ff591d3fb1d0525ca981a9816ec
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2023-01-15 17:45:27 +0330

    Update Searchline.php

--- app/Services/Searchline/Searchline.php
+++ app/Services/Searchline/Searchline.php
@@ -12,7 +12,35 @@ class Searchline
     }
 
     public function isMobileBelongsToPerson($mobile,$nationalCode){
+
         return true;
+        header('Content-Type: text/html; charset=utf-8');
+		$mobile = array("09332999173");
+		$idcode = array("0890345775");
+		$parametr = array(
+		'token'=>env("SEARCHLINE_TOKEN"),
+		'Mobile'=>json_encode($mobile),
+		'IdCode'=>json_encode($idcode),
+		'op'=>'Shahkar');
+		$handler = curl_init("https://inquery.ir/:80");
+		curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($handler, CURLOPT_POSTFIELDS, $parametr);
+		curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($handler);
+		$result = json_decode($response, true);
+        $httpcode = curl_getinfo($handler, CURLINFO_HTTP_CODE);
+        dd($httpcode,$result);
+
+        echo $result["Result"][0]["Validation"];
+        echo $result["Result"][1]["Validation"];
+
+
+
+
+
+
+
+
         $data = [
             "Mobile"=>array($mobile),
             "IdCode"=>array($nationalCode)
