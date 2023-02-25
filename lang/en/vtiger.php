62c42372abb9aa9df4bdd4dd686fdf83111eb88c
hoseinalizadeh1373 <hoseinalizadehkhiabani@gmail.com>
2023-02-25 18:50:57 +0330

    docs trans

--- lang/en/vtiger.php
+++ lang/en/vtiger.php
@@ -2,4 +2,7 @@
 
 return [
     "national_image" => "تصویر کارت ملی",
+    "Lease_image"=> "تصویر اجاره نامه یا سند",
+    "certificate_image" => "تصویر صفحه 1 و 2 شناسنامه",
+    "bad_background_image" => "تصویر عدم سوء پیشینه"
 ];
