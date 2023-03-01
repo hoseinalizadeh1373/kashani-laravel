522dd8e130795091254f553aa58466ca13157e70
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2023-03-01 15:26:38 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get("/hook",function(Request $request){
+    Log::info($request->get("text"),$request->all());
 });
