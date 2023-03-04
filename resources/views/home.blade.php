dd07b9cc45004f2c9c24d005a484fc0f9c2c78e2
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2023-03-04 09:39:49 +0330

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             به پنل مرکز پرستاری ثمین خوش آمدید
-
             <div class="col-md-4">
                 @auth
                 <a href="{{url('/client/form')}}">جهت ویرایش اطلاعات خود کلیک کنید</a>
