28f9e8321fd755b2651d07408ae6c1a500093f6c
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2023-03-16 10:13:07 +0330

    loading

--- resources/views/vtiger-forms/_partials/_modal.blade.php
+++ resources/views/vtiger-forms/_partials/_modal.blade.php
@@ -5,16 +5,17 @@
   {{-- <div class="loaderProgress"></div> --}}
 
  {{-- <div class="loader"></div> --}}
- <div class="container2">
+ {{-- <div class="container2">
   <div class="span3">
     <div class="circle">
     </div>
   </div>
-</div>
+</div> --}}
+<img src="/img/loading.gif" width="250" height="250" alt="">
 
 
  
-  <h5>لطفا تا پایان عملیات منتظر بمانید...</h5>  
+  <h5 class="text-warning">لطفا تا پایان عملیات منتظر بمانید...</h5>  
   </div>
 
 </div>
