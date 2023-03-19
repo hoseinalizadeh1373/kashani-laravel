7107578091cc6a993000336513f9ce910f9abe9e
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2023-03-19 11:28:39 +0330

    f

--- resources/views/vtiger-forms/_partials/_documentMoragheb.blade.php
+++ resources/views/vtiger-forms/_partials/_documentMoragheb.blade.php
@@ -94,11 +94,18 @@
       </tr>  
      </tr>    --}}
 
+
      <form id="upload_moragheb_asnad" action="{{ url ('/createDocument')}}"  method="post" enctype="multipart/form-data">
       @csrf
       {{-- @method('POST') --}}
-      {{-- <div id="div"class="alert alert-success fade show h6 small p-2' d-none" role='alert' >  </div> --}}
+      
 <tr id="tr">
+<td>
+  <div id="div"class="alert alert-success fade show h6 small p-2' d-none" role='alert' >اسناد بارگذاری شده تاکنون: </div>
+</td>
+</tr>
+<tr>
+  
   <td > 
      <select id="select_asnad" name="upload_file" data-label="label:%D9%88%D8%B6%D8%B9%DB%8C%D8%AA+%D8%AA%D8%A7%D9%87%D9%84" required="required" pattern="">
       <option value="">انتخاب نوع سند</option>
