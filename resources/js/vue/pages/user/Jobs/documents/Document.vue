95fd4285e37675a9641fc4e1e526a4ab2f3e08f0
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2023-04-17 18:53:22 +0330

    pdf download

--- resources/js/vue/pages/user/Jobs/documents/Document.vue
+++ resources/js/vue/pages/user/Jobs/documents/Document.vue
@@ -1,18 +1,23 @@
 <template >
-  <v-container>asasasd
+  <v-container>
     <v-row justify="center">
       <v-col class="py-0 ma-0" md="8">
         <v-dialog v-model="dialog" persistent>
           <card :loading="loading" title="مشاهده سند">
             <v-card-text v-if="doc">
-              <v-container> 
-                <h4>{{  document.notes_title }}</h4>
-                <v-img :src="docImage" max-height="300" />
+              <v-container>
+                <h4>{{ document.notes_title }}</h4>
+                <v-img :src="docImage" max-height="300" v-if="isImage" />
+                <v-btn  block v-if="isPdf" color="primary" :href="docPdf" :download="document.notes_title + '.pdf'">دانلود سند pdf</v-btn>
               </v-container>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="$emit('cancel')">
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="$emit('cancel')"
+              >
                 انصراف
               </v-btn>
             </v-card-actions>
@@ -45,17 +50,64 @@ export default {
     user() {
       return this.auth.user;
     },
-    docImage(){
+    docImage() {
       return "data:image/jpeg;base64, " + this.doc.filepath;
-    }
+    },
+    docPdf() {
+      return "data:application/octet-stream;base64, " + this.doc.filepath;
+    },
+    isImage() {
+      return ["image/jpeg", "image/png"].includes(this.document.filetype);
+    },
+    isPdf() {
+      return this.document.filetype == "application/pdf";
+    },
   },
   methods: {
     async getDoc() {
       this.loading = true;
-      const { data } = await axios.get(`/api/users/${this.user.id}/documents/${this.document.id}`);
+      const { data } = await axios.get(
+        `/api/users/${this.user.id}/documents/${this.document.id}`
+      );
       this.doc = data;
-      console.log(data)
       this.loading = false;
+    },
+    b64toBlob(b64Data) {
+      const contentType = "application/pdf";
+      var sliceSize = 512;
+      b64Data = b64Data.replace(/^[^,]+,/, "");
+      b64Data = b64Data.replace(/\s/g, "");
+      var byteCharacters = window.atob(b64Data);
+      var byteArrays = [];
+
+      for (
+        var offset = 0;
+        offset < byteCharacters.length;
+        offset += sliceSize
+      ) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+          byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+      }
+
+      var blob = new Blob(byteArrays, { type: contentType });
+      return blob;
+    },
+    downloadPdf() {
+      const blob = this.b64toBlob(this.doc.filepath);
+      console.log(blob)
+      if (window.saveAs) {
+        window.saveAs(blob, name);
+      } else {
+        navigator.saveBlob(blob, name);
+      }
     },
   },
   async mounted() {
